f69dc01ae23f2ea7b10870b423b2e5e0724b0bcd
Yanda Huang <realyanda@hey.com>
2021-11-14 14:09:16 -0800

    New changes with mkdocs

--- pi_setup_interactive.sh
+++ pi_setup_interactive.sh
@@ -42,3 +42,8 @@ sudo pip3 install docker-compose
 # TailScale
 curl -fsSL https://tailscale.com/install.sh | sh
 
+# https://docs.linuxserver.io/faq#libseccomp
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 648ACFD622F3D138
+echo "deb http://deb.debian.org/debian buster-backports main" | sudo tee -a /etc/apt/sources.list.d/buster-backports.list
+sudo apt update
+sudo apt install -t buster-backports libseccomp2
